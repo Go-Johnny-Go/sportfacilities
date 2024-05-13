eebc9423ad1d4caa5b9acb5ba7a3f5c053f6f4fd
Jhonny Iriarte <iriarte.condori.jhonny25@gamil.com>
2024-05-13 16:47:34 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1 className="text-4xl font-extrabold underline text-red-600">
+          Hello world!
+        </h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
